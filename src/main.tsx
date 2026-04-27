e4c4c042e44d9d9b51b06a4dc907729bfc1b2e1a
laptop <mrakemail@protonmail.com>
2026-04-27 15:11:02 +0100

    feat: wire up app routing with ThemeProvider, WalletProvider, and all four page routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,22 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
+import { theme } from './theme'
+import { WalletProvider } from './context/WalletContext'
+import App from './App'
 
-const rootElement = document.getElementById('root')
-if (!rootElement) throw new Error('Root element #root not found in document.')
+const root = document.getElementById('root')
+if (!root) throw new Error('Root element not found')
 
-createRoot(rootElement).render(
+createRoot(root).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <WalletProvider>
+          <App />
+        </WalletProvider>
+      </ThemeProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
